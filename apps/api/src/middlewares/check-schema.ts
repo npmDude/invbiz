aa49756df5d47f22265ddd1b30d2557dbf5bf181
Neil Patrick Mendoza <37074771+npmDude@users.noreply.github.com>
2026-09-06 07:53:04 +0800

    Update check-schema.ts

--- apps/api/src/middlewares/check-schema.ts
+++ apps/api/src/middlewares/check-schema.ts
@@ -2,12 +2,22 @@ import type { NextFunction, Request, Response } from 'express';
 import { z } from 'zod';
 
 interface Input {
+  paramsSchema?: z.ZodType;
   querySchema?: z.ZodType;
   bodySchema?: z.ZodType;
 }
 
-export function checkSchema({ querySchema, bodySchema }: Input) {
+export function checkSchema({ paramsSchema, querySchema, bodySchema }: Input) {
   return (req: Request, _res: Response, next: NextFunction) => {
+    if (paramsSchema) {
+      const result = paramsSchema.safeParse(req.params);
+
+      if (!result.success) {
+        next(result.error);
+        return;
+      }
+    }
+
     if (querySchema) {
       const result = querySchema.safeParse(req.query);
 
