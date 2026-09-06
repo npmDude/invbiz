e603ae557f50fd5027d6de94f1256cabd0e087e6
Neil Patrick Mendoza <37074771+npmDude@users.noreply.github.com>
2026-09-07 04:31:48 +0800

    Update routes.ts

--- apps/api/src/routes.ts
+++ apps/api/src/routes.ts
@@ -1,11 +1,13 @@
 import type { Express } from 'express';
 
 import authRouter from './modules/auth/auth.controller';
+import branchesRouter from './modules/branches/branches.controller';
 import organizationsRouter from './modules/organizations/organizations.controller';
 import usersRouter from './modules/users/users.controller';
 
 export function setupRoutes(app: Express): void {
   app.use('/auth', authRouter);
+  app.use('/branches', branchesRouter);
   app.use('/organizations', organizationsRouter);
   app.use('/users', usersRouter);
 }
