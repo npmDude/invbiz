a838539a2f1ef177d32abc5491c4ec322b469985
Neil Patrick Mendoza <37074771+npmDude@users.noreply.github.com>
2026-09-11 22:52:46 +0800

    Update db.ts

--- apps/api/test/helpers/db.ts
+++ apps/api/test/helpers/db.ts
@@ -18,6 +18,7 @@ export async function resetDatabase() {
       users_branches,
       roles_permissions,
       products_categories,
+      branches_products,
       products,
       branches,
       categories,
